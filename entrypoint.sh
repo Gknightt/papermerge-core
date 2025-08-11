8e26255f449cb3477e21b46e3fc2262d4c470917
Rivo Vebayo <86941434+Gknightt@users.noreply.github.com>
2025-08-11 23:18:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,5 @@ echo "Running database migrations..."
 poetry run task migrate
 
 echo "Starting Papermerge server..."
-poetry run task server
 
 poetry run uvicorn docker.__cloud__obsolete.config.asgi:application --host 0.0.0.0 --port ${PORT:-8000}
